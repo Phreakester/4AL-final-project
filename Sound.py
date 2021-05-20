fc6b7d9559483e75b8d3164e092091629ae20bc3
Phreakester <getty58@me.com>
2021-05-20 13:45:34 -0700

    Got sample audio files, added audio playing functionality to Sound class using openAL, but no positional audio yet

--- Sound.py
+++ Sound.py
@@ -1,20 +1,25 @@
 import time
 import random
 import keyboard
+import openal
 
 class Sound():
-  def __init__(self, position = 90, sound = None):
+  def __init__(self, position = 90, sound_path = None):
     self.position = position
-    if sound:
-      self.sound = sound
+    if sound_path:
+      self.sound_path = sound_path
     else:
-      self.sound = 'DEFAULT FILE PATH'
+      self.sound_path = './sounds/cymbal.wav'
+
+    self.source = openal.oalOpen(self.sound_path)
 
     self.response_time = None
+
   def playSound(self):
     #Play audio from library, using self.position
     #NOTE: Only plays sound, does not collect data
-    print("Playing sound...")
+    print("Playing sound... at: ", self.position)
+    self.source.play() #Play sound
   def test(self):
     initial_time = time.time()
     self.playSound()
@@ -24,6 +29,7 @@ class Sound():
         keyboard.wait('m')
     final_time = time.time()
     self.response_time = final_time - initial_time
+    openal.oalQuit()
 
 if __name__ == "__main__":
     print("Running two sounds, one in front of you (z) and one to the right")
