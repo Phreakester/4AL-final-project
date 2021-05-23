0604a892bab8f79859b74ace256ab4ab886664ac
Gautier404 <and.jgautier@gmail.com>
2021-05-22 17:06:30 -0700

    finish sound.py

--- Sound.py
+++ Sound.py
@@ -2,6 +2,7 @@ import time
 import random
 import keyboard
 import openal
+import numpy as np
 
 class Sound():
   def __init__(self, position = 90, sound_path = None):
@@ -9,7 +10,7 @@ class Sound():
     if sound_path:
       self.sound_path = sound_path
     else:
-      self.sound_path = './sounds/cymbal.wav'
+      self.sound_path = './sounds/cymbalnew.wav'
 
     self.source = openal.oalOpen(self.sound_path)
 
@@ -18,8 +19,12 @@ class Sound():
   def playSound(self):
     #Play audio from library, using self.position
     #NOTE: Only plays sound, does not collect data
-    print("Playing sound... at: ", self.position)
+    xpos = -5*np.sin(self.position*np.pi/180)
+    zpos = -5*np.cos(self.position*np.pi/180)
+    print("Playing sound... at: ", self.position , " degrees from straight ahead")
+    self.source.set_position((xpos,0,zpos ))
     self.source.play() #Play sound
+
   def test(self):
     initial_time = time.time()
     self.playSound()
@@ -35,7 +40,7 @@ if __name__ == "__main__":
     print("Running two sounds, one in front of you (z) and one to the right")
     test1 = Sound()
     test1.test()
-    test2 = Sound(40)
+    test2 = Sound(-90)
     test2.test()
     print(test1.response_time)
     print(test2.response_time)
